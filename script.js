a985be6aff1316a44ca93945579a550ddf812581
Valerie <valisfis@gmail.com>
2022-04-04 03:27:24 +0200

    finisched Project

--- script.js
+++ script.js
@@ -1,52 +1,73 @@
-const main = document.querySelector('main');
 const buttonRock = document.querySelector('#rock');
 const buttonPaper = document.querySelector('#paper');
 const buttonScissors = document.querySelector('#scissors');
-
+const main = document.querySelector('main');
 let userWins = 0;
 let computerWins = 0;
 let tie = 0;
-const template = `
-        <p>Wins: ${userWins}</p>
-        <p>Losses: ${computerWins}</p>
-        <p>Ties: ${tie}</p>
-    `;
-main.innerHTML = template.join('');
- 
+let text = " ";
+const count = function(){ 
+    const template = `
+     <p>Wins: ${userWins}</p>
+     <p>Losses: ${computerWins}</p>
+     <p>Ties: ${tie}</p>
+     <p>${text}</p>
 
-let userChoice;
-buttonRock.addEventListener("click", function(){
-    userChoice = "r";
-})
-buttonPaper.addEventListener("click", function(){
-    userChoice = "p";
-})
-buttonScissors.addEventListener("click", function(){
-    userChoice = "s";
-})
+`;
+main.innerHTML = template;
+}
+count();
 
-let randomNumber = Math.floor(Math.random() * 3);
-console.log (randomNumber);
-let computerChoice;
-    if (randomNumber === 0) {
-        computerChoice = "r";
-    } else if (randomNumber === 1){
-        computerChoice = "p";
-    } else {
-        computerChoice ="s";
-    }
-console.log (computerChoice);
-
+const play = function(userChoice){
+    let randomNumber = Math.floor(Math.random() * 3);
+    //console.log (randomNumber);
+    let computerChoice;
+        if (randomNumber === 0) {
+            computerChoice = "Rock";
+        } else if (randomNumber === 1){
+            computerChoice = "Paper";
+        } else {
+            computerChoice ="Scissors";
+        }
+    console.log (computerChoice);
     
-if (computerChoice === "r" && userChoice === "p") {
+    if (computerChoice === "Rock" && userChoice === "Paper") {
         userWins++;
-    } else if (computerChoice === "p" && userChoice === "s"){
+        text = "You won! " + userChoice + " beats " + computerChoice + ".";
+    } else if (computerChoice === "Paper" && userChoice === "Scissors"){
+        text = "You won! " + userChoice + " beats " + computerChoice + ".";
         userWins++;
-    } else if (computerChoice === "s" && userChoice === "r"){
+    } else if (computerChoice === "Scissors" && userChoice === "Rock"){
         userWins++;
+        text = "You won! " + userChoice + " beats " + computerChoice + ".";
     } else if (computerChoice === userChoice){
         tie++;
+        text = "It's a tie.";
     } else {
         computerWins++;
+        text = "You lost. The computer chose " + computerChoice + " which beats " + userChoice + ".";
     }
+
+console.log ("Wins:" + userWins);
+console.log ("Ties:" + tie);
+console.log ("Losses:" + computerWins);
+count();
 }
+
+buttonRock.addEventListener("click", function(){
+    console.log ("Rock");
+    play("Rock");
+})
+buttonPaper.addEventListener("click", function(){
+    console.log ("Paper");
+    play("Paper");
+})
+buttonScissors.addEventListener("click", function(){
+    console.log ("Scissors");
+    play("Scissors");
+})
+
+
+
+
+    
